6dd1b01e860e6190306c278d671e213008630acb
Jomar Granada <jomart.granada@gmail.com>
2023-09-17 00:32:21 +0800

    added login initial code

--- src/App.js
+++ src/App.js
@@ -1,7 +1,43 @@
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { Container } from 'react-bootstrap'
+import { UserProvider } from './UserContext';
+import { useState, useEffect } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
 function App() {
+
+  const [ user, setUser ] = useState ({
+    loginKey: null
+  })
+
+  const unsetUser = () => {
+    localStorage.clear();
+  }
+
+  useEffect( () => {
+
+  fetch('http://localhost:4000/users/login', {
+    headers: {
+      authorization: `Bearer ${ localStorage.getItem ('token')}`
+    }
+  })
+  .then(res => res.json())
+  .then(data => {
+    console.log(data);
+    if (data === true) {
+      setUser ({
+        loginKey: true
+      })
+    } else {
+      setUser ({
+        loginKey: false
+      })}
+      
+  })
+
+  })
+
   return (
     <div className="App">
       <header className="App-header">
